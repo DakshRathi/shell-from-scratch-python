9ba97af36cf3aed9ff41297a268cb0554d41ee13
DakshRathi <117447434+DakshRathi@users.noreply.github.com>
2025-10-29 00:44:29 +0530

    Implemented exit

--- app/main.py
+++ app/main.py
@@ -2,12 +2,26 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    command = input()
-    while command != "exit":
-        print(f"{command}: command not found")
+    while True:
         sys.stdout.write("$ ")
-        command = input()
+        sys.stdout.flush()
+
+        command_line = input()
+        parts = command_line.split()
+
+        if not parts:
+            continue
+
+        command = parts[0]
+
+        if command == "exit":
+            if len(parts) > 1:
+                exit_code = int(parts[1])
+                sys.exit(exit_code)
+            else:
+                sys.exit(0)
+        else:
+            print(f"{command_line}: command not found")
 
 
 if __name__ == "__main__":
