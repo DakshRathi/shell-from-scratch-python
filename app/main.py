e47d96f46d26122e136370e2c87ed65339150dc2
DakshRathi <117447434+DakshRathi@users.noreply.github.com>
2025-10-29 00:18:36 +0530

    Implemented Exit

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ import sys
 
 def main():
     sys.stdout.write("$ ")
-    while True:
-        # Wait for user input
-        command = input()
+    command = input()
+    while command != "exit":
         print(f"{command}: command not found")
         sys.stdout.write("$ ")
+        command = input()
 
 
 if __name__ == "__main__":
