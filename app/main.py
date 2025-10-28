d6adf6264d4de16a0e3802238e153208fbb8a47b
DakshRathi <117447434+DakshRathi@users.noreply.github.com>
2025-10-28 15:45:21 +0530

    Handle Invalid Commands

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ import sys
 def main():
     sys.stdout.write("$ ")
 
+    # Wait for user input
+    command = input()
+    print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
